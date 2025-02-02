a56e726ecf2ff72a46e461a6b0baed0f2e466559
Efosa Igbinehi <liova2273@gmail.com>
2025-02-02 12:18:49 +0000

    removed unused import, added a custon icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
 import { Activity } from 'lucide-react';
